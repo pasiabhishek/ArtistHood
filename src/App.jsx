ccaf03c0d2f3663de24ca0976d10c1d5706b93df
pasi <pasiabhishek6070@gmail.com>
2026-07-07 01:53:47 +0530

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,50 @@ import './App.css'
 
 
 function App() {
-      <div>
+  return (
+     <div className="hero">
+      <nav className="nav">
+        <div className="logo">
+          ARTIST<span>HOOD</span>
         </div>
+        <div className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>Artist</li>
+            <li>Categories</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+        <div className="logsign">
+          <buttonc className="login">Login</buttonc>
+          <button className="signup">Get Started</button>
+        </div>
+      </nav>
+      <div className="hero-content">
+        <div className="hero-text">
+          <p className="hero-tagline">
+            India's Premium Artist Booking Platform.
+          </p>
+          <h1>
+            BOOK YOUR <br /> FAVORITE
+            <br />
+            <span>ARTIST...</span>
+          </h1>
+          <p className="hero-para">
+            Discover and look top singers , DJs , dancers and performers for your events.
+          </p>
+        </div>
+        <div className="blank">
+
+        </div>
+      </div>
+    </div>
+
+
+  
+  )
 
 }
 
