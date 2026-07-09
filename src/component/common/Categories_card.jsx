d60dceee8324cd392dc424fddc1d7ef4e6a97b31
pasi <pasiabhishek6070@gmail.com>
2026-07-09 21:01:12 +0530

    Update Categories_card.jsx

--- src/component/common/Categories_card.jsx
+++ src/component/common/Categories_card.jsx
@@ -6,7 +6,7 @@ export default function Categories_card() {
     const TopCats = [
         { id: 1, icon: "https://img.magnific.com/premium-vector/vector-man-singing-with-simple-silhouette-style_995281-6793.jpg?semt=ais_hybrid&w=740&q=80", name: "Singer" },
         { id: 2, icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-zN60eTq73yNjDzZciqu7mEbMXwOsBxhnEurla9p4xA&s=10", name: "Dancer" },
-        { id: 3, icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw51L3R_JM_mEFqv7mRPe2R9m9gbP4HURNc2fJjd0PSg&s=10", name: "singer" },
+        { id: 3, icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw51L3R_JM_mEFqv7mRPe2R9m9gbP4HURNc2fJjd0PSg&s=10", name: "DJ" },
         { id: 4, icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOK8LoZqQYiaMxwEMsVpVZYGtKqjEM2T_Ig6THZTTOtW2d_0XLxWmZ0oM&s=10", name: "Band" },
         { id: 5, icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFu21-PKZcA5b6Fk1gvDVjxNe54dkjpIkOMLC3zeu1AA&s=10", name: "Actor" },
         { id: 6, icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTalT3WLvB7_UysyLpsUXP7-d2EWvv0A5tlFZ-hDziKyQ&s=10", name: "Anchor" }
