51698065914aafd72e32a16fb7668e3ddb063cf4
pasi <pasiabhishek6070@gmail.com>
2026-07-10 19:54:45 +0530

    Update Why_AH.jsx

--- FrontEnd/src/component/common/Why_AH.jsx
+++ FrontEnd/src/component/common/Why_AH.jsx
@@ -12,7 +12,7 @@ export default function why_AH() {
         </h3>
       </div >
       <div className="Why_Reason w-100vw h-40 m-10 ">
-        <div className="app_features w-50 h-20 flex border-1 m-10 md:w-50 h-25 " >
+        <div className="app_features w-50 h-20 flex border-1 m-10 md:(w-50 h-25) " >
           <div className="img w-50 h-25 p-10 m-6 flex justify-center items-center">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgMcA4FrZEPIocT2ssTCRiFYWb2_M_V8b4Ss_oFh2xIw&s=10" alt="" srcset="" className='w-30 h-20 md:w-30' />
           </div>
