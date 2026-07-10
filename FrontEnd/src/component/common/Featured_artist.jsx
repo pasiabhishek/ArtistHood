170e398f0567454093cd7f77ec761f1f4085d391
pasi <pasiabhishek6070@gmail.com>
2026-07-10 22:33:53 +0530

    ok

--- FrontEnd/src/component/common/Featured_artist.jsx
+++ FrontEnd/src/component/common/Featured_artist.jsx
@@ -29,10 +29,37 @@ export default function Featured_artist() {
 
 
 export function Featured_artist_card() {
+    let feature_json =[
+        {
+            "id": 1,
+            "title": "Verified Artists",
+            "description": "All artists are verified and quality checked.",
+            "icon": "verified"
+        },
+        {
+            "id": 2,
+            "title": "Secure Payments",
+            "description": "100% secure payments with multiple options.",
+            "icon": "payments"
+        },
+        {
+            "id": 3,
+            "title": "Instant Booking",
+            "description": "Books instantly and get confirmation in seconds.",
+            "icon": "booking"
+        },
+        {
+            "id": 4,
+            "title": "24/7 Support",
+            "description": "Our team is always here to help you.",
+            "icon": "support"
+        }
+    ]
 
     return (
 
         <div className='artist_body'>
+            
             {artists.map((Artist_api) => (
 
                 <div className="artist_card" key={Artist_api.id}>
--- FrontEnd/src/component/pages/Home.jsx
+++ FrontEnd/src/component/pages/Home.jsx
@@ -15,6 +15,7 @@ export default function Home() {
             <Categories_card />
             <Featured_artist />
             <Why_AH />
+            
 
         </div>
     )
