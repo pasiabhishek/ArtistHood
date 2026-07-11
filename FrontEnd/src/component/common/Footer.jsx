2401f58ad52147fb73ba9c06c1c7191cd12521dc
pasi <pasiabhishek6070@gmail.com>
2026-07-12 00:20:41 +0530

    Working on Footer

--- FrontEnd/src/component/common/Footer.jsx
+++ FrontEnd/src/component/common/Footer.jsx
@@ -7,8 +7,8 @@ import { IoLogoWhatsapp } from "react-icons/io";
 
 export default function Footer() {
     return (
-        <div style={{ margin: " 40px auto 2px auto " }} className="footer_body w-100vw bg-gray-950 flex justify-center items-center flex-col sm:flex-row h-50 ">
-            <div className="inner_conatiner flex justify-around items-center flex-col sm:flex-row h-30 ">
+        <div style={{ margin: " 40px auto 2px auto " }} className="footer_body w-100vw bg-gray-950 flex justify-center items-center flex-col sm:flex-row sm:h-50 ">
+            <div className="inner_conatiner flex justify-around items-center flex-col sm:flex-row sm:h-30 ">
 
                 {/* first coloum */}
                 <div className="firstCol m-10 gap-1.5  items-center  w-30 text-center">
