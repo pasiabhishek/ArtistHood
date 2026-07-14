7dce5e81815d3af983f90b3183b6a1c07a055611
pasi <pasiabhishek6070@gmail.com>
2026-07-14 21:26:13 +0530

    working on login

--- FrontEnd/src/component/pages/Login.jsx
+++ FrontEnd/src/component/pages/Login.jsx
@@ -11,22 +11,22 @@ export default function Login() {
                 </div>
             </nav>
 
-            <div className="m-10 " style={{margin:"0px 50px"}}>
-                <form className="w-[40vw] h-110  justify-center items-center flex bg-red-400 flex-col rounded-3xl">
-                    <h2 className="text-3xl font-bold">Login</h2>
+            <div className="w-[250px] md:w-[400px] h-110  justify-center items-center flex flex-col rounded-3xl bg-gray-950 border-2 border-red-50 text-amber-50" style={{ margin: "50px" }}>
+                <form className="w-[250px] md:w-[400px] h-110  flex flex-col rounded-3xl bg-gray-950 border-2 border-red-50" style={{ padding: "10px" }}>
+                    <h2 className="text-[20px] font-bold">Welcome back !</h2>
                     <input
                         type="text"
                         id="username"
                         placeholder="Username"
                         className="w-50 border-2 bg-amber-600"
                     />
-<input
+                    <input
                         type="text"
                         id="username"
                         placeholder="Username"
                         className="w-50 border-2 bg-amber-600"
                     />
-<input
+                    <input
                         type="text"
                         id="username"
                         placeholder="Username"
