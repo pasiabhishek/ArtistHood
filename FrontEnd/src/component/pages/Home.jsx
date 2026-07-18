176943dfb83270ba73d94aa70140e2ebe28035ed
pasi <pasiabhishek6070@gmail.com>
2026-07-18 21:52:20 +0530

    routing added

--- FrontEnd/src/component/pages/Home.jsx
+++ FrontEnd/src/component/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Home.css'
-import Header from '../common/Header'
 import Hero from '../common/Hero'
 import Categories_card from '../common/Categories_card';
 import Featured_artist from '../common/Featured_artist';
