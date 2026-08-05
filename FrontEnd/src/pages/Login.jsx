eb0bfffaa0a3555bd2bc2ba5e5e3dc856f2dc7d0
pasi <pasiabhishek6070@gmail.com>
2026-08-05 19:40:34 +0530

    updated

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./css/Auth.css";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://artist-hood.vercel.app";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export default function Login() {
     const navigate = useNavigate();
