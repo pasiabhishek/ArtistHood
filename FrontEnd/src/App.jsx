3d77575d9986bcf587f86ba0fb709f6244cf833d
pasi <pasiabhishek6070@gmail.com>
2026-08-17 20:53:19 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="*" element={<NotFound />} />
-                    <Route path="/feed" element={<AfterLogin />} />
+                    <Route path="/feed" element={<Feed />} />
 
                     <Route element={<AppLayout />}>
                         <Route path="/" element={<Home />} />
