eaa34a0b4863938a278baeeda79826b17be9e969
pasi <pasiabhishek6070@gmail.com>
2026-08-19 23:39:26 +0530

    starting main post feed

--- FrontEnd/src/components/feed/Right_nav.jsx
+++ FrontEnd/src/components/feed/Right_nav.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 export default function Right_nav() {
-  return (
-    <div>
-      <div className="Right_Nav">
+    return (
+        <div>
+            <div className="Right_Nav">
+                <div className="Right_Nav_Header">
 
-      </div>
-    </div>
-  )
+                </div>
+
+            </div>
+        </div>
+    )
 }
