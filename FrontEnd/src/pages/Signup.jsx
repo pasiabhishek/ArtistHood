78481444b47168e946f0bc4343aa4b9593b7ae12
pasi <pasiabhishek6070@gmail.com>
2026-08-22 01:07:21 +0530

    Update Signup.jsx

--- FrontEnd/src/pages/Signup.jsx
+++ FrontEnd/src/pages/Signup.jsx
@@ -40,6 +40,7 @@ export default function Signup() {
             localStorage.setItem("token", res.data.user.token);
             localStorage.setItem("user", JSON.stringify(res.data.user));
             alert("Account Created");
+            
             navigate("/feed");
         } catch (err) {
             alert(err.response?.data?.message || "Signup Failed");
