becbc82c6a3d901464cd571b96fc50ced0356c4b
Vikas <vikasrjc7@gmail.com>
2026-08-23 23:45:55 +0530

    add artist-profile routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getMe } = require('../controllers/authController');
+const { artistProfile } = require('../controllers/artistController');
 const { protect } = require('../middleware/auth');
 
 router.post('/register', registerUser);
+router.post('/artist-profile', protect, artistProfile);
 router.post('/login', loginUser);
 router.get('/me', protect, getMe);
 
