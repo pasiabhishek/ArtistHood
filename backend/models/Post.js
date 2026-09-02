7e220ca9cbdebb89123c4ed1efb51fc283008824
Vikas <vikasrjc7@gmail.com>
2026-09-02 21:58:33 +0530

    update id for portfolio

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -4,6 +4,6 @@ const PortfolioSchema = new mongoose.Schema({
     title: { type: String, required: true },
     url: { type: String, required: true },
     description: { type: String, default: '' },
-}, { _id: false });
+}, { _id: true, timestamps: true });
 
 module.exports = PortfolioSchema;
