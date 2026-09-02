e77e5a209fec268d4576fed97597d98563c0a424
Vikas <vikasrjc7@gmail.com>
2026-09-02 23:00:03 +0530

    update upload for memoryStorage

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,27 +1,10 @@
 const multer = require("multer");
-const fs = require("fs");
-const path = require("path");
 
-const storage = multer.diskStorage({
-    destination: "uploads/",
-    filename: (req, file, cb) => {
-        const extension = path.extname(file.originalname);
-        const name = path.basename(file.originalname, extension);
-        let count = 1;
-        let filename = `${name}${extension}`;
-
-        while (fs.existsSync(path.join("uploads", filename))) {
-            filename = `${name}(${count})${extension}`;
-            count++;
-        }
-        cb(null, filename);
-    }
-});
-
+const storage = multer.memoryStorage();
 
 const upload = multer({
     storage,
-    limits: { fileSize: 10 * 1024 * 1024, },
+    limits: { fileSize: 50 * 1024 * 1024, },
 });
 
 module.exports = upload;
