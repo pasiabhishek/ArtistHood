8fe3a55735864f65dc64cc30c220f02e0c007b01
Vikas <vikasrjc7@gmail.com>
2026-09-02 21:29:21 +0530

    add file storage to disk using multer

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,10 +1,27 @@
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: (req, file, cb) => {
+        const extension = path.extname(file.originalname);
+        const name = path.basename(file.originalname, extension);
+        let count = 1;
+        let filename = `${name}${extension}`;
+
+        while (fs.existsSync(path.join("uploads", filename))) {
+            filename = `${name}(${count})${extension}`;
+            count++;
+        }
+        cb(null, filename);
+    }
+});
+
 
 const upload = multer({
     storage,
-    limits: { fileSize: 50 * 1024 * 1024, },
+    limits: { fileSize: 10 * 1024 * 1024, },
 });
 
 module.exports = upload;
