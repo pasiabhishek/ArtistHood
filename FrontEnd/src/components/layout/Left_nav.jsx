b78dfaa6bca3a74ccae30a3b7e4b55db5e9d3e36
pasi <pasiabhishek6070@gmail.com>
2026-09-05 15:17:15 +0530

    Refactor Left_nav component and sidebar CSS
    Rework Left_nav.jsx markup and styles: simplify JSX, remove nested Links by wrapping <Link> around <li>, add explicit nav items (Home, Discover, Booking, Messages, Notification, Create Post), use stored user displayName and update profile image path. Clean up and consolidate Feed.css for left/right sidebars: deduplicate rules, add box-sizing, icon/hover styles, create-post and footer styling, and responsive breakpoints. Overall improves structure, accessibility, and responsive behavior of the sidebar.

--- FrontEnd/src/components/layout/Left_nav.jsx
+++ FrontEnd/src/components/layout/Left_nav.jsx
@@ -1,63 +1,80 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Left_nav() {
-
     const storedUser = localStorage.getItem("user");
     const user = storedUser ? JSON.parse(storedUser) : null;
+
     const displayName = user?.fullName || "there";
-    const email = user?.email || "";
+
     return (
-        <div>
-            <div className="Left_Nav">
-                <div className="Left_Nav_header">
-                    <div className="logo">
-                        ARTIST<span className="">HOOD</span>
-                    </div>
-                </div>
-                <div className="Left_Nav_Navbar">
-                    <ul>
-                        <li>
-                            <Link to="/feed">
-                                <i className="fa-regular fa-house"></i> Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/discover">
-                                <i className="fa-solid fa-users"></i> Discover
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/booking">
-                                <i className="fa-regular fa-calendar-check"></i> Booking
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/messages">
-                                <i className="fa-solid fa-message"></i> Messages
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/notifications">
-                                <i className="fa-regular fa-bell"></i> Notification
-                            </Link>
-                        </li>
-                        <Link to="/create-post">
-                            <li>
-                                <button>
-                                    + Create Post
-                                </button>
-                            </li>
-                        </Link>
+        <div className="Left_Nav">
 
-                    </ul>
+            {/* Header */}
+            <div className="Left_Nav_header">
+                <div className="logo">
+                    ARTIST<span>HOOD</span>
                 </div>
-                <div className="Left_Nav_Footer">
-                    <img src="favicon2.png" alt="profile picture" />
-                    <h3>{displayName}</h3>
-                </div>
-
             </div>
+
+            {/* Navbar */}
+            <div className="Left_Nav_Navbar">
+                <ul>
+
+                    <Link to="/feed">
+                        <li>
+                            <i className="fa-regular fa-house"></i>
+                            Home
+                        </li>
+                    </Link>
+
+                    <Link to="/discover">
+                        <li>
+                            <i className="fa-solid fa-users"></i>
+                            Discover
+                        </li>
+                    </Link>
+
+                    <Link to="/booking">
+                        <li>
+                            <i className="fa-regular fa-calendar-check"></i>
+                            Booking
+                        </li>
+                    </Link>
+
+                    <Link to="/messages">
+                        <li>
+                            <i className="fa-solid fa-message"></i>
+                            Messages
+                        </li>
+                    </Link>
+
+                    <Link to="/notifications">
+                        <li>
+                            <i className="fa-regular fa-bell"></i>
+                            Notification
+                        </li>
+                    </Link>
+
+                    <Link to="/create-post">
+                        <li>
+                            + Create Post
+                        </li>
+                    </Link>
+
+                </ul>
+            </div>
+
+            {/* Footer */}
+            <div className="Left_Nav_Footer">
+                <img
+                    src="/favicon2.png"
+                    alt="profile"
+                />
+
+                <h3>{displayName}</h3>
+            </div>
+
         </div>
-    )
+    );
 }
