4ef34c587898af1beba911e28e2a4789a2d60037
pasi <pasiabhishek6070@gmail.com>
2026-09-09 22:35:47 +0530

    Update AppLayout.jsx

--- FrontEnd/src/components/layout/AppLayout.jsx
+++ FrontEnd/src/components/layout/AppLayout.jsx
@@ -14,7 +14,7 @@ export default function AppLayout() {
             <LeftNav />
             <Outlet />
             <RightNav />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 }
