dba1607b8a3f05ddb19505f4c3769d2414e460a3
Vikas <vikasrjc7@gmail.com>
2026-09-15 11:00:08 +0530

    add get my artist profile endpoint

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getMe } = require('../controllers/authController');
-const { createArtistProfile } = require('../controllers/artistController');
+const { createArtistProfile, getMyArtistProfile } = require('../controllers/artistController');
 const { protect } = require('../middleware/auth');
+
 
 router.post('/register', registerUser);
 router.post('/artist-signup', protect, createArtistProfile);
+router.get('/artist-profile', protect, getMyArtistProfile);
 router.post('/login', loginUser);
 router.get('/me', protect, getMe);
 
