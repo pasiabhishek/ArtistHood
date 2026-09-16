a0dd3fb838d0b0b1718ccc3c05e4da1c3a161dba
Vikas <vikasrjc7@gmail.com>
2026-09-16 21:02:11 +0530

    add booking schema

---
+++ backend/models/Booking.js
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+    {
+        client: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        artist: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        eventDate: {
+            type: Date,
+            required: true,
+        },
+
+        startTime: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        endTime: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        eventType: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        expectedGuests: {
+            type: Number,
+            required: true,
+            min: 1,
+        },
+
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 2000,
+        },
+
+        price: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+
+        status: {
+            type: String,
+            enum: [
+                "pending",
+                "accepted",
+                "rejected",
+                "confirmed",
+                "completed",
+                "cancelled",
+            ],
+            default: "pending",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+//for artist booking queries
+bookingSchema.index({
+    artist: 1,
+    eventDate: 1,
+    status: 1,
+});
+
+// for client booking history
+bookingSchema.index({
+    client: 1,
+    createdAt: -1,
+});
+
+export default mongoose.model("Booking", bookingSchema);
