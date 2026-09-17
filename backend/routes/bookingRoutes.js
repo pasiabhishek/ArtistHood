58f4f569e49b9fc9e8d5d6f97eff70e702a4cf06
Vikas <vikasrjc7@gmail.com>
2026-09-17 18:28:48 +0530

    add get client booking routes

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { protect } = require('../middleware/authMiddleware');
-const { createBooking, getArtistBookings } = require('../controllers/bookingController');
+const { createBooking, getArtistBookings, getClientBookings } = require('../controllers/bookingController');
 
 // Apply  protect middleware to all routes
 router.use(protect);
 
 router.post('/create-booking', createBooking);
-router.get('/', getArtistBookings);
+router.get('/client-bookings', getClientBookings);
+router.get('/artist-bookings', getArtistBookings);
 
 module.exports = router;
