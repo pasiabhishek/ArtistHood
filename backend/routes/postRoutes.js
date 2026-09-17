10ec5e170a0f1c576e17d8d4b4e26bb952b0c666
Vikas <vikasrjc7@gmail.com>
2026-09-17 14:15:49 +0530

    add route for get all post

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createPost } = require('../controllers/postController');
+const { createPost, getAllPosts } = require('../controllers/postController');
 const upload = require('../middleware/uploadMiddleware');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/create', protect, upload.single('media'), createPost);
+router.get('/', getAllPosts);
 
 module.exports = router;
