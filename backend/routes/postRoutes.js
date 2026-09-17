1e4d1393574de33f46426a133639fbe84aba4a2a
Vikas <vikasrjc7@gmail.com>
2026-09-17 14:36:05 +0530

    add routes of get post by username

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createPost, getAllPosts } = require('../controllers/postController');
+const { createPost, getAllPosts, getPostsByArtist } = require('../controllers/postController');
 const upload = require('../middleware/uploadMiddleware');
 const { protect } = require('../middleware/authMiddleware');
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/create', protect, upload.single('media'), createPost);
 router.get('/', getAllPosts);
+router.get('/:username', protect, getPostsByArtist);
 
 module.exports = router;
