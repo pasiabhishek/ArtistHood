9be5b058d3a35832f2dd45b8de09b531394bf243
Vikas <vikasrjc7@gmail.com>
2026-09-17 20:38:32 +0530

    add accept booking route

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const { protect } = require('../middleware/authMiddleware');
-const { createBooking, getArtistBookings, getClientBookings, getBookingById } = require('../controllers/bookingController');
+const { createBooking, getArtistBookings, getClientBookings,
+    getBookingById, acceptBooking } = require('../controllers/bookingController');
 
 // Apply  protect middleware to all routes
 router.use(protect);
@@ -10,6 +11,7 @@ router.use(protect);
 router.post('/create-booking', createBooking);
 router.get('/client-bookings', getClientBookings);
 router.get('/artist-bookings', getArtistBookings);
-router.get('/:bookingId', getBookingById);
+router.get('/:id', getBookingById);
+router.put('/:id/accept', acceptBooking);
 
 module.exports = router;
